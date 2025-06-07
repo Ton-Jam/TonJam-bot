5a8b697c52b54ce3a438a5d6c39c317dfae0a81b
Ton-Jam <info.tonjam@gmail.com>
2025-06-07 17:57:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Environment variables
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # e.g., https://tonjambot.onrender.com
+BOT_TOKEN = os.getenv("7591465695:AAFMdgh2tCD7nNvLG2DrODjy7wg8MvEWVoA")
+WEBHOOK_URL = os.getenvhttps://tonjambot.onrender.com  
 PORT = int(os.getenv("PORT", 8443))  # Render assigns PORT dynamically
 
 # Initialize DB
